1105d3a2685c0ad14e7fdae0f14bd7aeb58ac285
Pradeepani <pradeepani@hsenidoutsourcing.com>
2026-03-16 23:10:26 +0530

    Adding Seating Plan and Link to Row from Search

--- src/commonUtils.js
+++ src/commonUtils.js
@@ -1,4 +1,4 @@
-export function parseCSV(csvText) {
+export function parseCSVtoJSON(csvText) {
     const rows = csvText.split(/\r?\n/); // Split CSV text into rows, handling '\r' characters
     const data = []; // Initialize an array to store parsed data
     for (let i = 1; i < rows.length; i++) {
@@ -10,4 +10,35 @@ export function parseCSV(csvText) {
         data.push(rowObject);
     }
     return data;
+}
+
+export function parseCSVtoOrderedList(csvText) {
+    var rows = csvText.split(/\r?\n/);
+    const data = [];
+    rows = rows.slice(1);
+    rows = rows.sort((a, b) => a.localeCompare(b));
+    for (let i = 0; i < rows.length; i++) {
+        const rowData = rows[i].split(',');
+        const rowObject = {
+            letterGroup: rowData[0].charAt(0),
+            name: rowData[0],
+            table: rowData[1]
+        };
+        data.push(rowObject);
+    }
+
+    const groupedResult = Map.groupBy(data, ({letterGroup}) => letterGroup);
+
+    const groupedArray = [];
+
+    groupedResult.entries().forEach(entry => {
+        groupedArray.push(
+            {
+                letter: entry[0],
+                nameList: entry[1]
+            }
+        );
+    });
+
+    return groupedArray;
 }
