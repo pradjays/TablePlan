aa50e33ea2db387e50487bf11390071b64c246a0
Pradeepani <pradeepani@hsenidoutsourcing.com>
2026-05-02 16:57:07 +0530

    Adding line breaks for Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div className="container display-flex vw-100">
             <div className="row fst-italic cursor typewriter-animation font-elegant">
-                <div className="font-size-l fw-bolder">Pradeepani and Prabodha</div>
+                <div className="font-size-l fw-bolder">Pradeepani <br/> and <br/> Prabodha</div>
             </div>
             <div className="row fst-italic cursor typewriter-animation font-elegant delayed-animation-1">
                 <div className="font-size-s fw-bolder">06th May 2026</div>
@@ -13,7 +13,7 @@ function Home() {
             </div>
             <br/>
             <div className="row fst-italic cursor typewriter-animation font-elegant delayed-animation-3">
-                <div className="font-size-m fw-bolder">(Click "Search" in the top left corner <br/> to find your seat)</div>
+                <div className="font-size-s fw-bolder">(Click "Search" <br/> in the top left corner <br/> to find your seat)</div>
             </div>
         </div>
     );
