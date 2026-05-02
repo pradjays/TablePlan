c252ca418361f356025c05ea1c8257fdf00ec9df
Pradeepani <pradeepani@hsenidoutsourcing.com>
2026-05-02 16:50:21 +0530

    Changing styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <div className="container">
+        <div className="container display-flex vw-100">
             <div className="row fst-italic cursor typewriter-animation font-elegant">
                 <div className="font-size-l fw-bolder">Pradeepani and Prabodha</div>
             </div>
@@ -12,8 +12,8 @@ function Home() {
                 <div className="font-size-m fw-bolder">Let's find your seat...</div>
             </div>
             <br/>
-            <div className="row fst-italic cursor typewriter-animation font-elegant delayed-animation-2">
-                <div className="font-size-m fw-bolder">(Click "Search" in the top left corner to find your seat)</div>
+            <div className="row fst-italic cursor typewriter-animation font-elegant delayed-animation-3">
+                <div className="font-size-m fw-bolder">(Click "Search" in the top left corner <br/> to find your seat)</div>
             </div>
         </div>
     );
